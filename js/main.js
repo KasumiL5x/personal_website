f26f0871b2b687a12ea0ff9bb8cf6bf6782057ad
Daniel Green <KasumiL5x@gmail.com>
2016-09-08 08:08:56 +0100

    Made scrolling bars into view activate them, and also to do so if they are initially visible.

--- js/main.js
+++ js/main.js
@@ -10,15 +10,50 @@ $('.header__mobile-nav-toggle').on('click', function() {
   }
 });
 
-$(document).ready(function() {
-  // test: init only specific progress bars
-  // var bars = [];
-  // $('.ng-progress').each(function(){
-  //   if( parseInt($(this).attr('ng-percent')) >= 90 ) {
-  //     bars.push($(this));
-  //   }
-  // });
-  // ng.initProgressBars(bars);
+var Utils = {
+  isElementInView: function(element, fullyInView, screenBias) {
+    var pageTop = $(window).scrollTop() - ($(window).height() * screenBias);
+    var pageBottom = pageTop + $(window).height();
+    var elementTop = $(element).offset().top;
+    var elementBottom = elementTop + $(element).height();
 
-  ng.initAllProgressBars();
+    if( fullyInView === true ) {
+      return ((pageTop < elementTop) && (pageBottom > elementBottom));
+    } else {
+      return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+    }
+  }
+};
+
+var Page = {};
+// gather all activations
+Page.activations = [];
+$('.skills-list').each(function() {
+  Page.activations.push({
+    triggerObject: $(this), // object to trigger activation
+    objects: $(this).find('.ng-progress'), // objects to do something to
+    activated: false // whether or not this activation has been activated; TODO: Remove instead of using this bool check!
+  });
+});
+
+Page.activateOnScroll = function() {
+  var yPos = window.pageYOffset;
+  for (var i = 0; i < this.activations.length; i++) {
+    var curr = this.activations[i];
+    var inView = Utils.isElementInView(curr.triggerObject, false, 0);
+    if( !curr.activated && inView ) {
+      // console.log('Activating: ' + curr.objects);
+      curr.activated = true;
+      ng.initProgressBars(curr.objects);
+    }
+  }
+};
+
+$(window).on('scroll', function() {
+  Page.activateOnScroll();
+});
+
+$(document).ready(function() {
+  // try to activate upon load in case elements are already visible
+  Page.activateOnScroll();
 });
